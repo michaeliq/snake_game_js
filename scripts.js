43b9fa18fa6400cc944bdbe642483b1c98de1749
josué castillo <michaelesacol@gmail.com>
2021-05-04 08:38:52 -0500

    new branch walk through interval

--- scripts.js
+++ scripts.js
@@ -1,27 +1,34 @@
-document.addEventListener('DOMContentLoaded',()=>{
 
+document.addEventListener('DOMContentLoaded',()=>{
+     
+     const score = document.querySelector('#score');
+     score.innerHTML = 0;
+     const level = document.querySelector('#level');
      let grid = document.querySelector('.grid');
-     let score = document.querySelector('#score');
-     let level = document.querySelector('#level');
      let snake = new Snake(45,46);
      let width = 10;
      
      let squareFragment = crateBoard();
      grid.appendChild(squareFragment);
-
      let squares = document.querySelectorAll('.grid div');
 
+
      snake.printSnake(squares);
+     createApple(squares);
      let eventLoop = setInterval(()=>{
-          createApple(squares);
-          //
-          //snake.moveSnakeUp(squares,width);
-     },1000);
+          appleWasEaten(squares);
+          
+     },200);
 
      document.addEventListener('keyup',(e)=>{
+          moveSnake(e);
+     });
+
+     //move the snake
+     function moveSnake(e){
+
           switch(e.code) {
                case 'ArrowUp':
-                    console.log('up');
                     snake.moveSnakeUp(squares,width);
                     break;
                case 'ArrowDown':
@@ -34,15 +41,13 @@ document.addEventListener('DOMContentLoaded',()=>{
                     snake.moveSnakeRight(squares,width);
                     break;
           }
-     });
+     }
      
 });
 
-//move the snake
 
 //create the apple
 const createApple = (board)=>{
-     cleanBoard(board);
      let idCellChosen = Math.round(Math.random() * 99);
      board[idCellChosen].classList.add('apple');
 }
@@ -53,8 +58,10 @@ class Snake{
           this.head = head;
           this.body = [];
           this.tail = tail;
-          this.isVertical = false;
-          this.isHorizontal = true;
+          this.isUp = true;
+          this.isDown = true;
+          this.isRight = true;
+          this.isLeft = true;
      }
 
      growBody(headId){
@@ -62,40 +69,70 @@ class Snake{
      }
 
      printSnake(board){
-          board[this.head].classList.add('snake');
           board[this.tail].classList.add('snake');
+          board[this.head].classList.add('snake');
      }
 
      moveSnakeUp(board,width){
-          if(this.isVertical === false){ 
+          if(this.isUp === true){
+               let oldHead = this.head;
+               let oldTail = this.tail;
                this.head -= width;
+               this.tail = oldHead;
                this.printSnake(board);
-               this.isVertical = true;
-               this.isHorizontal = false;
+               board[oldTail].classList.remove('snake');
+               if(this.isUp){
+                    this.isDown = false;
+                    this.isRight = true;
+                    this.isLeft = true;      
+              }
           }
      }
      moveSnakeDown(board,width){
-          if(this.isVertical === false){ 
+          if(this.isDown === true){ 
+               let oldHead = this.head;
+               let oldTail = this.tail;
                this.head += width;
-               this.printSnake(board)
-               this.isVertical = true;
-               this.isHorizontal = false;
+               this.tail = oldHead;
+               board[oldTail].classList.remove('snake');
+               this.printSnake(board);
+               if(this.isUp) {
+                    this.isUp = false;
+                    this.isRight = true;
+                    this.isLeft = true;
+               }
+               
           }
      }
-     moveSnakeLeft(board,width){
-          if(this.isHorizontal === false){ 
+     moveSnakeLeft(board){
+          if(this.isLeft === true){ 
+               let oldHead = this.head;
+               let oldTail = this.tail;
                this.head -= 1;
+               this.tail = oldHead;
+               board[oldTail].classList.remove('snake');
                this.printSnake(board);
-               this.isVertical = false;
-               this.isHorizontal = true;
+               if(this.isLeft) {
+                    this.isUp = true;
+                    this.isDown = true;
+                    this.isRight = false;
+               }
           }
      }
-     moveSnakeRight(board,width){
-          if(this.isHorizontal === false){ 
+     moveSnakeRight(board){
+          if(this.isRight === true){ 
+               let oldHead = this.head;
+               let oldTail = this.tail;
                this.head += 1;
+               this.tail = oldHead;
+               board[oldTail].classList.remove('snake');
                this.printSnake(board);
-               this.isVertical = false;
-               this.isHorizontal = true;
+               if(this.isRight){
+                    this.isLeft = false;
+                    this.isUp =true;
+                    this.isDown = true;
+               }
+               
           }
      }
 
@@ -106,16 +143,29 @@ const crateBoard = ()=>{
      for(let i = 0; i < 100; i++){
           let square = document.createElement('div');
           square.setAttribute('class','cell');
+          square.setAttribute('id',i);
           fragment.appendChild(square);
      }
      return fragment
 }
-//catch evants 'snake ate apple' on the board
+//catch events 'snake ate apple' on the board
+const appleWasEaten = (board)=>{
+     board.forEach((cell)=>{
+          if(cell.classList.contains('snake') && cell.classList.contains('apple')){
+               cleanBoard(board);
+               createApple(board);
+               newScore = parseInt(score.innerHTML) + 1;
+               score.innerHTML = newScore;
+          }
+     })
+}
+
 function cleanBoard(board){
      board.forEach((cell)=>{
           if(cell.classList.contains('apple')) cell.classList.remove('apple');
      });
 }
+
 //catch collition of snake on walls
 //create loop to evaluate all events
 
